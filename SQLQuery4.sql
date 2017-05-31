e8a88e5ecb333fb265f18736b9226f3e2b0352a6
hsinfeng <judy4a031004@gmail.com>
2017-05-31 21:09:26 +0800

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,84 @@
+USE JUDYも穘都基ヘ
+go
+
+CREATE TABLE JUDYも穘都基ヘ (
+    荡珇跋 int,
+    狹基 varchar(10),
+    い狹基 varchar(10),
+    ず甧s varchar(20),
+    荐秖 varchar(10) 
+);
+
+select * from JUDYも穘都基ヘ
+
+DROP TABLE JUDYも穘都基ヘ;
+
+CREATE TABLE JUDYも穘都基ヘ (
+    荡珇跋 varchar(10),
+    狹基 varchar(10),
+    い狹基 varchar(10),
+    ず甧 varchar(20),
+    荐秖 varchar(10) 
+);
+
+select * from JUDYも穘都基ヘ
+
+DROP TABLE JUDYも穘都基ヘ;
+
+CREATE TABLE JUDYも穘都基ヘ (
+    荡珇跋 varchar(255),
+    狹基 varchar(255),
+    い狹基 varchar(255),
+    ず甧 varchar(255),
+    荐秖 varchar(255) 
+);
+
+select * from JUDYも穘都基ヘ
+
+INSERT INTO JUDYも穘都基ヘ (荡珇跋, 狹基, い狹基, ず甧, 荐秖)
+VALUES ('┷礟',25,20,'拉','0/100g' ),
+        ('┷礟厚',25, 20,'璠','0/100g' ),
+		('玜瓁獵',25,20,'蔼獵','0/100g' ),
+		('Νナ',25,20,'ナ','20/100g' ),
+		('ヌ',50,35,'ρ','11.5/100g');
+
+select * from JUDYも穘都基ヘ
+go
+
+UPDATE "JUDYも穘都基ヘ"
+SET 狹基=25
+
+select * from JUDYも穘都基ヘ
+
+UPDATE JUDYも穘都基ヘ
+SET 狹基=45
+where 荡珇跋='ヌ'
+
+select * from JUDYも穘都基ヘ
+
+INSERT INTO JUDYも穘都基ヘ (荡珇跋, 狹基, い狹基, ず甧, 荐秖)
+VALUES ('痌イ',50,35,'里','60/100g' );
+
+select * from JUDYも穘都基ヘ
+
+DELETE FROM JUDYも穘都基ヘ
+WHERE 荡珇跋='痌イ';
+
+select * from JUDYも穘都基ヘ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
